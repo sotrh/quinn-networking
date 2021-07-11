74115416f32e23d8e98ec01faae7b962267a7342
Ben Hansen <bhbenjaminhansen@gmail.com>
2021-07-11 17:13:27 -0600

    client not hanging

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use futures::StreamExt;
+use quinn::ReadError;
 use quinn_networking::{client, server};
 
 
@@ -10,34 +11,47 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Server
     tokio::spawn(async move {
         let incoming_conn = incoming.next().await.unwrap();
-        let new_conn = incoming_conn.await.unwrap();
+        let mut new_conn = incoming_conn.await.unwrap();
         println!(
             "[server] connection accepted: addr={}",
             new_conn.connection.remote_address(),
         );
         
-        let (mut send, mut recv) = new_conn.connection.open_bi().await.unwrap();
+        let (mut send, mut recv) = new_conn.bi_streams.next().await.unwrap().unwrap();
 
         send.write_all(b"test").await.unwrap();
 
         let mut buffer = vec![];
-        while let Ok(op_received) = recv.read(&mut buffer).await {
-            if let Some(received) = op_received {
-                println!("[server] received {} bytes", received);
-                match &buffer[..] {
-                    b"ping" => {
-                        println!("[server] received \"ping\" sending \"pong\"");
-                        send.write_all(b"pong").await.unwrap();
+        loop {
+            match recv.read(&mut buffer).await {
+                Ok(Some(received)) => {
+                    println!("[server] received {} bytes", received);
+                    match &buffer[..] {
+                        b"ping" => {
+                            println!("[server] received \"ping\" sending \"pong\"");
+                            send.write_all(b"pong").await.unwrap();
+                        }
+                        b"marco" => {
+                            println!("[server] received \"marco\" sending \"polo\"");
+                            send.write_all(b"polo").await.unwrap();
+                        }
+                        it => println!("[server] Unknown message {:?}", it),
                     }
-                    b"marco" => {
-                        println!("[server] received \"marco\" sending \"polo\"");
-                        send.write_all(b"polo").await.unwrap();
-                    }
-                    it => println!("[server] Unknown message {:?}", it),
+                }
+                Ok(None) => {
+                    println!("[server] no data received");
+                }
+                Err(ReadError::ConnectionClosed(_)) => { 
+                    println!("connection closed");
+                    break;
+                }
+                Err(e) => {
+                    println!("an error occurred: {:?}", e);
+                    break;
                 }
             }
-
         }
+
 
         println!("[server] closing");
         send.finish().await.unwrap();
@@ -47,7 +61,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let endpoint = client("0.0.0.0:0".parse().unwrap(), &[&server_cert])?;
     let quinn::NewConnection {
         connection,
-        mut bi_streams,
         ..
     } = endpoint
         .connect(&server_addr, "localhost")
@@ -56,7 +69,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
     println!("[client] connected: addr={}", connection.remote_address());
 
-    let (mut send, mut recv) = bi_streams.next().await.unwrap()?;
+    let (mut send, mut recv) = connection.open_bi().await.unwrap();
 
     println!("[client] sending \"ping\"");
     send.write_all(b"ping").await?;
@@ -69,14 +82,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("[client] received {} bytes", received);
         println!("[client] received message {}", std::str::from_utf8(&buffer)?);
     } else {
-        eprintln!("Unable to receive message from server")
+        eprintln!("[client] unable to receive message from server")
     }
     if let Ok(Some(received)) = recv.read(&mut buffer).await {
         println!("[client] received {} bytes", received);
         println!("[client] received message {}", std::str::from_utf8(&buffer)?);
     }
     else {
-        eprintln!("Unable to receive message from server")
+        eprintln!("[client] unable to receive message from server")
     }
 
     endpoint.wait_idle().await;
